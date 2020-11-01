975fd939414a947aba758ac2bf85d25ad5158f5b
Alex E <tyrael596@gmail.com>
2020-11-01 17:11:31 +0100

    ole ole los caracole

---
+++ src/main/java/edu/upc/etsetb/archsoft/spreadsheet/blocks/Function.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.archsoft.spreadsheet.blocks;
+
+/**
+ *
+ * @author Alex
+ */
+public class Function {
+    
+}
---
+++ src/main/java/edu/upc/etsetb/archsoft/spreadsheet/formulas/MAX.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upc.etsetb.archsoft.spreadsheet.formulas;
+
+/**
+ *
+ * @author Alex
+ */
+public class MAX {
+    
+}
